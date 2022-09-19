ebf4ba58c6b67ba3897b7d2c0c4a5206ac1c4be8
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-09-18 21:49:31 -0400

    created delete request

--- server.js
+++ server.js
@@ -19,10 +19,14 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
+// Gets notes stored json data
 app.get("/api/notes", (req, res) => {
-  res.json(db);
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    err ? console.log(err) : res.json(JSON.parse(data));
+  });
 });
 
+// Posts notes to json data
 app.post("/api/notes", (req, res) => {
   const { title, text } = req.body;
 
@@ -31,14 +35,14 @@ app.post("/api/notes", (req, res) => {
     const newNote = {
       title,
       text,
-      id: uniqueId(`${title}-`),
+      id: uniqueId(`note-`),
     };
 
     // Write the string to a file
     fs.readFile("./db/db.json", "utf-8", (err, data) => {
       err ? console.log(err) : data;
 
-      //   Parse the database json object array
+      //   Parse the database array
       const originalData = JSON.parse(data);
 
       //   Push the new note into the array
@@ -62,6 +66,43 @@ app.post("/api/notes", (req, res) => {
   } else {
     res.status(500).json("Error in posting review");
   }
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    err ? console.log(err) : data;
+    const array = JSON.parse(data);
+
+    const chosenNote = array.find((element) => element.id === req.params.id);
+    console.log(chosenNote);
+    if (!chosenNote) {
+      res.status(404).json({
+        status: "fail",
+        message: "Invalid Id",
+      });
+    } else {
+      const index = array.indexOf(chosenNote);
+      array.splice(index, 1);
+
+      fs.writeFile("./db/db.json", JSON.stringify(array), (err) =>
+        err ? console.error(err) : res.json(array)
+      );
+    }
+    // array.forEach((element) => {
+    //   const currentId = element.id;
+    //   const selectedId = req.params.id;
+    //   //   console.log(currentId, selectedId);
+
+    //   if (currentId == selectedId) {
+    //     return res.status(204).json({
+    //       status: "success",
+    //       message: `${element} was deleted!`,
+    //     });
+    //   } else {
+
+    //   }
+    // });
+  });
 });
 
 app.listen(PORT, () => console.log(`Listening at PORT ${PORT}`));
